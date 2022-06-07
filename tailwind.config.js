096c3f71e88edfa25c9efe94e539f84c655ed144
easylearningbd <elebdsoft@gmail.com>
2022-06-08 01:51:57 +0600

    68 Customizing Container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   content: ["./*html"],
   theme: {
+    screens : {
+      sm : '576px',
+      md : '768px',
+      lg : '992px',
+      xl : '1200px',
+
+    },
+    container : {
+      center : true,
+      padding : '1rem'  // 16px
+    },
     extend: {
       fontFamily : {
         'poppins' : ['Poppins'],
